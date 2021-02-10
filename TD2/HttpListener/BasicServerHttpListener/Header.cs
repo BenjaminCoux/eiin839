2bc5a4aa047bc49fbf5dad503f179a49919039b6
Benjamin Coux <benjamin.coux@etu.unice.fr>
2021-02-10 16:40:03 +0100

    ajout question 1 http listener

---
+++ TD2/HttpListener/BasicServerHttpListener/Header.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicServerHTTPlistener
+{
+    class Header
+    {  
+        private System.Collections.Specialized.NameValueCollection response;
+
+
+        public Header(System.Collections.Specialized.NameValueCollection response)
+        {
+            this.response = response;
+        }
+        public Header()
+        {
+
+        }
+
+        public void setResponce(System.Collections.Specialized.NameValueCollection resp)
+        {
+            this.response = resp;
+        }
+
+        public void printHeader()
+        {
+            Console.WriteLine("****** VOICI LE HEADER *******");
+            Console.WriteLine(this.response);
+            Console.WriteLine("****** FIN DU HEADER *******");
+        }
+       
+    }
+}
