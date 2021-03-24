78bdd9bfc3fa6490f522a313363925c90fe08862
Benjamin Coux <benjamin.coux@etu.unice.fr>
2021-03-24 14:37:37 +0100

    ajout td5

---
+++ td5b/WcfServiceLibrary1/mathConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace mathConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathLibrary.IMathsOperations m = new MathLibrary.MathsOperationsClient();
+            Console.WriteLine(m.Add(2, 4));
+            Console.WriteLine(m.Substract(12, 5));
+            Console.WriteLine(m.Multiply(4, 5));
+            Console.ReadLine();
+
+        }
+    }
+}
